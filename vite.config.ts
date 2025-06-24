067564ecdb18ebace2c91a65c75824f371cc2f80
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2025-06-24 17:26:44 +0500

    Add 'base' to VIte config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/redesign-websitehouse-test-assignment/",
   plugins: [tailwindcss()],
 });
