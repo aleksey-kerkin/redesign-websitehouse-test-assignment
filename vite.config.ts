83bd391210e089f73d6c17ae7564214bc30bfb52
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2025-06-24 17:36:26 +0500

    Remove base from Vite config for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/redesign-websitehouse-test-assignment/",
   plugins: [tailwindcss()],
 });
